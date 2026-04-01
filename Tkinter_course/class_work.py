9008bf8fce408c7afb2a7b7802713b5ccbf36639
viktr <chukwuemekavictor693@gmail.com>
2026-04-01 12:18:15 +0100

    added the db

--- Tkinter_course/class_work.py
+++ Tkinter_course/class_work.py
@@ -1,11 +1,53 @@
 import tkinter as tk 
 from tkinter import ttk 
+import sqlite3
 
 
 
+
+def create_notebook_db():
+    #creating the db file 
+    conn = sqlite3.connect("note_book.db")
+    cursor = conn.cursor()
+    #create the table .
+    cursor.execute(""" CREAT TABLE IF NOT EXIST notes(id INTEGER PRIMARY KEY, content TEXT)""")
+    cursor.execute("SELECT COUNT(*) FROM notes")
+    if cursor.fetchone()[0] == 0:
+        cursor.execute("INSERT INTO notes (content) VALUES ('')")
+    conn.commit()
+    conn.close()
+
+
+
+
+def save_db(text):
+    #connect to the db 
+    conn = sqlite3.connect("note_book.db")
+    #control of the db
+    cursor = conn.cursor()
+    #execution of query 
+    cursor.execute(""" UPDATE notes SET content =? WHERE id = 1""", (text))
+    #to commit our query
+    conn.commit()
+    conn.close()
+
+
+
+def laod_db():
+    #connect to db 
+    conn = sqlite3.connect("note_book.db")
+    cursor = conn.cursor()
+    cursor.execute(""" SELECT content FROM notes WWHERE  id = 1""")
+    row  = cursor.fetchone()
+    #conn.commit()
+    conn.close()
+    return row[0] if  row else""
 
 
 def create_notebook_app():
+    create_notebook_db()
+
+    
     root = tk.Tk()
     root.title("Note Pad")
     root.geometry("400x300")

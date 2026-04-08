350828ee7fbe2a7fa8c51dc9a2fc2d157e4f869b
viktr <chukwuemekavictor693@gmail.com>
2026-04-08 08:58:03 +0100

    whatever man

--- advanced_py/decorators_and_generators/test.py
+++ advanced_py/decorators_and_generators/test.py
@@ -1,34 +1,36 @@
-def get_number_list(n):
-    result = []
-    for i in range(n):
-        result.append(i * i)
-        print(result[0])
-    return result 
-
-#num = get_number_list(1_000_000)
-#rint(num)
+import functools
+import time
 
 
-def get_number2(n):
-    for i in range(n):
-        yield i * i
+# create a simple function
+def greet():
+    return "Hello!"
 
-
-#num = get_number2(1_000_000)
-#print(next(num))
-#print(next(num))
-#print(next(num))
+# create var in to store the func
+say_hi  = greet
+#print(say_hi())
 
 
 
-def countdown(n):
-    print("Starting countdown...")
-    while n > 0:
-        yield n
-        n -= 1
-    print("Done!")
 
-gen = countdown(5)
-print(next(gen))
-print(next(gen))
-print(next(gen))
+#pass the func as arg 
+def run_t(func):
+    func()
+    func()
+    
+
+
+
+#print(run_t(greet))
+
+
+
+def make_greeter(name):
+    # the greeter func
+    def greeter():
+        return f"Hello, {name}"
+    return greeter 
+
+hello = make_greeter("alice")
+print(hello())
+
